837a20404d000bc56bc4c95141ad7ce2751ec7f2
Andrew Stevens <astevens@tstmedia.com>
2011-05-05 13:52:19 -0500

    sets up gallery partial rendering

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,6 +2,9 @@ class Gallery
   include Mongoid::Document
   include Mongoid::Timestamps # adds created_at and updated_at fields
   field :tags, :type => Array
+  field :description, :type => String
+  validates_presence_of :description, :message => "can't be blank"
+  
 
   has_many :medias
   belongs_to :user
