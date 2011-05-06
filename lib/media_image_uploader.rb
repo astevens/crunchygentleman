0db4ac2e442de90e109e25f438994bd250116e3a
Andrew Stevens <astevens@tstmedia.com>
2011-05-05 23:08:51 -0500

    adds new sizes

--- lib/media_image_uploader.rb
+++ lib/media_image_uploader.rb
@@ -34,7 +34,18 @@ class MediaImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files
   #
   version :thumb do
-    process :resize_to_fill => [100, 100]
+    process :resize_to_fill => [50, 50]
+  end
+  version :small do
+    process :resize_to_fit => [100, 100]
+  end
+  
+  version :medium do
+    process :resize_to_fit => [400, 400]
+  end
+  
+  version :large do
+    process :resize_to_fit => [900, 900]
   end
   
   ##
